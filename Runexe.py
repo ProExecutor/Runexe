75e5757ce62502c2af90c0e04bb77c25a82bfc6c
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2023-10-19 17:41:33 +0300

    Update Runexe.py

--- Runexe.py
+++ Runexe.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Path to your .exe file
-exe_path = "path_to_your_exe_file.exe"
+exe_path = "kincaid.exe"
 
 # Use subprocess.run() to execute the .exe file
 subprocess.run(exe_path)
