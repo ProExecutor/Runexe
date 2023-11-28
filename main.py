601a705f156b8dd2baa8044193f53f4d7de63878
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2023-11-28 12:25:38 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Import the necessary libraries
+import android
+
+# Create a new Android emulator instance
+droid = android.Android()
+
+# Display a message on the screen
+droid.makeToast("Hello, World!")
